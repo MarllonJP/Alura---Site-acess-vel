00c4d42e7b5be3d6437cc0d2818c97eeb989ebe9
MarllonJP <125297837+MarllonJP@users.noreply.github.com>
2025-03-18 13:45:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade')
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade')
+
+    botaoDeAcessibilidade.addEventListener('click', function (){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista')
+       })
+
     const aumentaFonteBotao document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao document.getElementById('diminuir-fonte');
 
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
@@ -7,4 +16,11 @@ document.addEventListener('DOMContentLoaded', function(){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+
+    })
+
 })
